cc69caa352c2ad927a029bef8f495f754e7438e7
gajmerkumar <gajmerk17@gmail.com>
2025-07-09 12:54:22 +0545

    Wed,Jul-9

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -11,7 +11,11 @@ export interface QuoteTable {
   quote: string;
 }
 
+// Here DB is our whole database schema/structure. It's a map that connects table names (users, quotes) to the shape of each table (UserTable, QuoteTable)
+// We have passed this 'DB' into Kysely in 'client.ts' (look there) which makes 'db' fully typed and safe.
 export interface DB {
   users: UserTable;
   quotes: QuoteTable;
 }
+
+// We use interface for defining the schema because Kysely supports (and expects) interface-based schema definitions.
